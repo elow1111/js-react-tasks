d0cefbce932dd95fe3b3fb61ea81a1d0f6432f7d
Максим <160880502+elow1111@users.noreply.github.com>
2024-10-10 16:16:23 +0300

    ListGroup.jsx

--- 7/src/ListGroup.jsx
+++ 7/src/ListGroup.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
+const ListGroup = ({ children }) => {
+  const listItems = React.Children.map(children, (child) => (
+    <li className="list-group-item">{child}</li>
+  ));
+  return (
+    <ul className="list-group">
+      {listItems}
+    </ul>
+  );
+};
 
+export default ListGroup;
 // END
