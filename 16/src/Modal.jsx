5a0f0e0eee7647b72cc76d79c6b168dde852b9ad
Максим <160880502+elow1111@users.noreply.github.com>
2024-10-10 16:21:48 +0300

    Modal.jsx

--- 16/src/Modal.jsx
+++ 16/src/Modal.jsx
@@ -3,4 +3,65 @@ import React from 'react';
 
 // BEGIN (write your solution here)
 
-// END
+import PropTypes from 'prop-types';
+
+const Modal = ({ isOpen, children }) => {
+  return (
+    <div
+      className={cn('modal', 'fade', { show: isOpen })}
+      style={{ display: isOpen ? 'block' : 'none' }}
+      role="dialog"
+      aria-modal="true"
+      aria-hidden={!isOpen}
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Modal.Header = ({ toggle, children }) => {
+  return (
+    <div className="modal-header">
+      <h5 className="modal-title">{children}</h5>
+      <button
+        type="button"
+        className="btn-close"
+        onClick={toggle}
+        aria-label="Close"
+      ></button>
+    </div>
+  );
+};
+
+Modal.Body = ({ children }) => {
+  return <div className="modal-body">{children}</div>;
+};
+
+Modal.Footer = ({ children }) => {
+  return <div className="modal-footer">{children}</div>;
+};
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+Modal.Header.propTypes = {
+  toggle: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+Modal.Body.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+Modal.Footer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Modal;
+// END 
