12b547f7bbb69e586625e5a8ed8f5e0074285bf9
Максим <160880502+elow1111@users.noreply.github.com>
2024-10-10 16:19:17 +0300

    Card.jsx

--- 14/src/Card.jsx
+++ 14/src/Card.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
-
+const Card = ({ children }) => {
+    return <div className="card">{children}</div>;
+  };
+  
+  Card.Body = ({ children }) => {
+    return <div className="card-body">{children}</div>;
+  };
+  
+  Card.Title = ({ children }) => {
+    return <h4 className="card-title">{children}</h4>;
+  };
+  
+  Card.Text = ({ children }) => {
+    return <p className="card-text">{children}</p>;
+  };
+  
+export default Card;
 // END
