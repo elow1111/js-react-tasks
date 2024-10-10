ad1c69903b5a9f964c9648f0c3264ce5b98a5757
Максим <160880502+elow1111@users.noreply.github.com>
2024-10-10 16:22:44 +0300

    Item.jsx

--- 18/src/Item.jsx
+++ 18/src/Item.jsx
@@ -2,4 +2,21 @@ import React from 'react';
 
 // BEGIN (write your solution here)
 
+const Item = ({ task, onClick }) => {
+  const { text, state } = task;
+  const isFinished = state === 'finished';
+
+  return (
+    <div className="row">
+      <div className="col-1">{task.id}</div>
+      <div className="col">
+        <a href="#" className="todo-task" onClick={onClick}>
+          {isFinished ? <s>{text}</s> : text}
+        </a>
+      </div>
+    </div>
+  );
+};
+  
+export default Item;
 // END
