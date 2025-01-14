4f45c2ef55d23d2db795bd198bf500f794a8a6c7
Максим <160880502+elow1111@users.noreply.github.com>
2025-01-14 13:19:11 +0300

    Progress.jsx

--- 5/src/Progress.jsx
+++ 5/src/Progress.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
-const Progress = ({ percentage }) => {
-    return (
-      <div className="progress">
-        <div
-          className="progress-bar"
-          role="progressbar"
-          aria-valuenow={percentage}
-          aria-valuemin="0"
-          aria-valuemax="100"
-          aria-label="progressbar"
-          style={{ width: `${percentage}%` }}
-        ></div>
-      </div>
-    );
-};
-export default Progress;
+export default class Progress extends React.Component
+{
+    render()
+    {
+        const {percentage} = this.props;
+        const widthStyle = {width: `${percentage}%`,}
+        return(
+            <div className="progress">
+            <div
+              className="progress-bar"
+              role="progressbar"
+              aria-valuenow={percentage}
+              aria-valuemin="0"
+              aria-valuemax="100"
+              aria-label="progressbar"
+              style={widthStyle}
+            ></div>
+          </div>
+        )
+    }
+}
 // END
