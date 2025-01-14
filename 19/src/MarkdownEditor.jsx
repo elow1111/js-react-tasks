0f23f783a02cd7930ad6458df0290ca4fccb4cdc
Максим <160880502+elow1111@users.noreply.github.com>
2025-01-14 13:27:34 +0300

    MarkdownEditor.jsx

--- 19/src/MarkdownEditor.jsx
+++ 19/src/MarkdownEditor.jsx
@@ -2,32 +2,30 @@ import React from 'react';
 import Editor from '@toast-ui/editor';
 
 // BEGIN (write your solution here)
+export default class MarkdownEditor extends React.Component
+{
+    constructor(props)
+    {
+        super(props)
+        this.MdRef = React.createRef()
+    }
+    componentDidMount()
+    {
+        const {onContentChange} = this.props
+        const editor = new Editor({
+            el: this.MdRef.current,
+            hideModeSwitch: true,
+          });
 
-class MarkdownEditor extends React.Component {
-  componentDidMount() {
-    const { onContentChange } = this.props;
-
-    this.editor = new Editor({
-      el: this.editorRef,
-      hideModeSwitch: true,
-    });
-
-    this.editor.addHook('change', () => {
-      const content = this.editor.getMarkdown();
-      onContentChange(content); 
-    });
-  }
-
-  componentWillUnmount() {
-
-    this.editor.destroy();
-  }
-
-  render() {
-    return (
-      <div ref={(el) => (this.editorRef = el)} />
-    );
-  }
+          editor.addHook("change", () => {
+            const content = editor.getMarkdown();
+            onContentChange(content)
+          });
+    }
+    render()
+    {
+        const {onContentChange} = this.props
+        return<div ref={this.MdRef}></div>
+    }
 }
-export default MarkdownEditor;
 // END
