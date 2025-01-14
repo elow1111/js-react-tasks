c09563b9336cc80711ef02bf3f50fa82d5f99d7c
Максим <160880502+elow1111@users.noreply.github.com>
2025-01-14 13:25:26 +0300

    Profile.jsx

--- 15/src/Profile.jsx
+++ 15/src/Profile.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
-
 import ThemeContext from './contexts';
 
 const content = 'Текст для вкладки Profile';
 
 class Profile extends React.Component {
-  // BEGIN (write your solution here)
   static contextType = ThemeContext;
 
   render() {
-    const { currentTheme } = this.context;
     return (
-      <article className={currentTheme.className}>
+      <article className={this.context.state.activeTheme}>
         {content}
       </article>
     );
   }
-  // END
 }
 
 export default Profile;
