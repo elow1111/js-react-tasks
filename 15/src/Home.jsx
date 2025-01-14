0ccae0f9b03fb4387aec8088165368f50b71fb0b
Максим <160880502+elow1111@users.noreply.github.com>
2025-01-14 13:25:11 +0300

    Home.jsx

--- 15/src/Home.jsx
+++ 15/src/Home.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
-
 import ThemeContext from './contexts';
 
 const content = 'Текст для вкладки Home';
 
 class Home extends React.Component {
-  // BEGIN (write your solution here)
   static contextType = ThemeContext;
 
   render() {
-    const { currentTheme } = this.context;
     return (
-      <article className={currentTheme.className}>
+      <article className={this.context.state.activeTheme}>
         {content}
       </article>
     );
   }
-  // END
 }
 
 export default Home;
