b9569c6f4eec315a3c2332fe17fbc05c31520c16
Максим <160880502+elow1111@users.noreply.github.com>
2025-01-14 13:20:30 +0300

    BtnGroup.jsx

--- 8/src/BtnGroup.jsx
+++ 8/src/BtnGroup.jsx
@@ -2,31 +2,35 @@ import cn from 'classnames';
 import React from 'react';
 
 // BEGIN (write your solution here)
-import { useState } from 'react';
-const BtnGroup = () => {
-    const [activeButton, setActiveButton] = useState(null);
-    const handleButtonClick = (button) => {
-        setActiveButton(button);
-    };
-    return (
-        <div className="btn-group" role="group">
-            <button
-                type="button"
-                className={cn('btn', 'btn-secondary', 'left', { active: activeButton === 'left' })}
-                onClick={() => handleButtonClick('left')}
-            >
-                Left
-            </button>
-            <button
-                type="button"
-                className={cn('btn', 'btn-secondary', 'right', { active: activeButton === 'right' })}
-                onClick={() => handleButtonClick('right')}
-            >
-                Right
-            </button>
-        </div>
-    );
-};
-
-export default BtnGroup;
+export default class BtnGroup extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {btnOneIsPressd: false, btnTwoIsPressd: false}
+    }
+    handleOneClick = () =>
+    {
+        this.setState({btnOneIsPressd: true, btnTwoIsPressd: false})
+    }
+    handleTwoClick = () =>
+        {
+            this.setState({btnOneIsPressd: false, btnTwoIsPressd: true})
+        }
+    render()
+    {
+        const btnClassOne = cn("btn", "btn-secondary", "left",{
+            'active': this.state.btnOneIsPressd,
+        })
+        const btnClassTwo = cn("btn", "btn-secondary", "right",{
+            'active': this.state.btnTwoIsPressd,
+        })
+        return(
+            <div className="btn-group" role="group">
+            <button type="button" className={btnClassOne} onClick={this.handleOneClick}>Left</button>
+            <button type="button" className={btnClassTwo} onClick={this.handleTwoClick}>Right</button>
+          </div>
+        )
+    }
+}
 // END
