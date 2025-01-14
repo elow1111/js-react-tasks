56d78ccf54c41e2791f2179c4e9b49687f1e34ca
Максим <160880502+elow1111@users.noreply.github.com>
2025-01-14 13:23:07 +0300

    Component.jsx

--- 12/src/Component.jsx
+++ 12/src/Component.jsx
@@ -1,49 +1,77 @@
-import get from 'lodash/get';
 import uniqueId from 'lodash/uniqueId';
 import React from 'react';
 
-// BEGIN (write your solution here)
-import { useState } from 'react';
-const Component = () => {
-    const [log, setLog] = useState([]);
-  
-    const handleAdd = () => {
-      const lastValue = get(log, [0], 0);
-      const newValue = lastValue + 1;
-      setLog([{ id: uniqueId(), value: newValue }, ...log]);
-    };
-  
-    const handleSubtract = () => {
-      const lastValue = get(log, [0], 0);
-      const newValue = lastValue - 1;
-      setLog([{ id: uniqueId(), value: newValue }, ...log]);
-    };
-  
-    const handleDelete = (id) => {
-      setLog(log.filter(item => item.id !== id));
-    };
-  
+export default class Component extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentValue: 0, logList: [] };
+  }
+
+  handleButtonClick = (isPositive) => {
+    this.setState((prevState) => {
+      
+      const lastValue = prevState.logList.length > 0 
+        ? prevState.logList[0].currentValue 
+        : 0;
+
+      const newValue = isPositive ? lastValue + 1 : lastValue - 1;
+
+      const newLog = { id: uniqueId(), currentValue: newValue };
+
+      return {
+        logList: [newLog, ...prevState.logList], 
+      };
+    });
+  };
+
+  handleLogButton = (deleteId) => {
+    this.setState((prevState) => {
+      const newLogList = prevState.logList.filter((item) => item.id !== deleteId);
+
+      return {
+        logList: newLogList,
+        currentValue: newLogList.length === 0 ? 0 : prevState.currentValue, 
+      };
+    });
+  };
+
+  render() {
+    const { logList } = this.state;
+
     return (
       <div>
         <div className="btn-group font-monospace" role="group">
-          <button type="button" className="btn btn-outline-success" onClick={handleAdd}>+</button>
-          <button type="button" className="btn btn-outline-danger" onClick={handleSubtract}>-</button>
+          <button 
+            onClick={() => this.handleButtonClick(true)} 
+            type="button" 
+            className="btn btn-outline-success"
+          >
+            +
+          </button>
+          <button 
+            onClick={() => this.handleButtonClick(false)} 
+            type="button" 
+            className="btn btn-outline-danger"
+          >
+            -
+          </button>
         </div>
-        <div className="list-group">
-          {log.map(item => (
-            <button
-              key={item.id}
-              type="button"
-              className="list-group-item list-group-item-action"
-              onClick={() => handleDelete(item.id)}
-            >
-              {item.value}
-            </button>
-          ))}
-        </div>
+
+        {logList.length > 0 && ( 
+          <div className="list-group">
+            {logList.map((logEl) => (
+              <button
+                key={logEl.id}
+                onClick={() => this.handleLogButton(logEl.id)} 
+                type="button"
+                className="list-group-item list-group-item-action"
+              >
+                {logEl.currentValue}
+              </button>
+            ))}
+          </div>
+        )}
       </div>
     );
-};
-  
-export default Component;
-// END
+  }
+}
