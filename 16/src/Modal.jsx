c25eec6a7bddffb02e98ed33789ad979cce2a694
Максим <160880502+elow1111@users.noreply.github.com>
2025-01-14 13:26:07 +0300

    Modal.jsx

--- 16/src/Modal.jsx
+++ 16/src/Modal.jsx
@@ -1,67 +1,51 @@
 import cn from 'classnames';
 import React from 'react';
 
-// BEGIN (write your solution here)
+const Header = (props) => (
+  <div className="modal-header">
+    <div className="modal-title">{props.children}</div>
+    <button
+      type="button"
+      className="btn-close"
+      data-bs-dismiss="modal"
+      aria-label="Close"
+      onClick={props.toggle}
+    ></button>
+  </div>
+);
 
-import PropTypes from 'prop-types';
+const Body = (props) => (
+  <div className="modal-body">
+    {props.children}
+  </div>
+);
 
-const Modal = ({ isOpen, children }) => {
-  return (
-    <div
-      className={cn('modal', 'fade', { show: isOpen })}
-      style={{ display: isOpen ? 'block' : 'none' }}
-      role="dialog"
-      aria-modal="true"
-      aria-hidden={!isOpen}
-    >
-      <div className="modal-dialog">
-        <div className="modal-content">
-          {children}
+const Footer = (props) => (
+  <div className="modal-footer">
+    {props.children}
+  </div>
+);
+
+export default class Modal extends React.Component {
+  static Header = Header;
+  static Body = Body;
+  static Footer = Footer;
+
+  render() {
+    const { isOpen, children } = this.props;
+    
+    const modalClass = cn("modal", { "fade show": isOpen });
+
+    return (
+      <div>
+        <div className={modalClass} style={{ display: isOpen ? "block" : "none" }} role="dialog">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              {children}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-Modal.Header = ({ toggle, children }) => {
-  return (
-    <div className="modal-header">
-      <h5 className="modal-title">{children}</h5>
-      <button
-        type="button"
-        className="btn-close"
-        onClick={toggle}
-        aria-label="Close"
-      ></button>
-    </div>
-  );
-};
-
-Modal.Body = ({ children }) => {
-  return <div className="modal-body">{children}</div>;
-};
-
-Modal.Footer = ({ children }) => {
-  return <div className="modal-footer">{children}</div>;
-};
-
-Modal.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
-  children: PropTypes.node.isRequired,
-};
-
-Modal.Header.propTypes = {
-  toggle: PropTypes.func.isRequired,
-  children: PropTypes.node.isRequired,
-};
-
-Modal.Body.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-Modal.Footer.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-export default Modal;
-// END 
+    );
+  }
+}
