39a1f78851c870c4c3daa23d6b856d7b3572f748
Максим <160880502+elow1111@users.noreply.github.com>
2025-01-14 13:21:18 +0300

    Carousel.jsx

--- 9/src/Carousel.jsx
+++ 9/src/Carousel.jsx
@@ -1,49 +1,66 @@
 import React from 'react';
 import cn from 'classnames';
 
-// BEGIN (write your solution here)
-import { useState } from 'react';
-import { uniqueId } from 'lodash';
+export default class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 }; // Начинаем с первого изображения (индекс 0)
+  }
 
-const Carousel = ({ images }) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const handleNext = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    };
-    const handlePrev = () => {
-        setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-    };
+  handleClick = (direction) => {
+    const { images } = this.props;
+    const { activeIndex } = this.state;
+
+    if (direction === "prev") {
+      // Если предыдущий индекс меньше 0, устанавливаем на последнее изображение
+      this.setState({
+        activeIndex: (activeIndex - 1 + images.length) % images.length
+      });
+    } else if (direction === "next") {
+      // Если следующий индекс больше последнего, возвращаемся к первому изображению
+      this.setState({
+        activeIndex: (activeIndex + 1) % images.length
+      });
+    }
+  }
+
+  render() {
+    const { images } = this.props;
+    const { activeIndex } = this.state;
+
     return (
-        <div id="carousel" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-inner">
-                {images.map((image, index) => (
-                    <div
-                        key={uniqueId('carousel-item')}
-                        className={cn('carousel-item', { active: index === currentIndex })}
-                    >
-                        <img alt="" className="d-block w-100" src={image} />
-                    </div>
-                ))}
+      <div id="carousel" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className={cn("carousel-item", { active: index === activeIndex })}
+            >
+              <img alt="" className="d-block w-100" src={image} />
             </div>
-            <button
-                className="carousel-control-prev"
-                type="button"
-                onClick={handlePrev}
-            >
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button
-                className="carousel-control-next"
-                type="button"
-                onClick={handleNext}
-            >
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
+          ))}
         </div>
+        <button
+          className="carousel-control-prev"
+          data-bs-target="#carousel"
+          type="button"
+          data-bs-slide="prev"
+          onClick={() => this.handleClick("prev")}
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          data-bs-target="#carousel"
+          type="button"
+          data-bs-slide="next"
+          onClick={() => this.handleClick("next")}
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     );
-};
-
-export default Carousel;
-// END
+  }
+}
